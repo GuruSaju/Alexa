866d90a85ea9a96068b5798fe14e39702b213598
Srisarguru Sridhar <srisargurusridhar@u.boisestate.edu>
2018-04-05 22:45:56 -0400

    Added more intents for guru skill - 20180405

--- AboutGuruSkill/index.js
+++ AboutGuruSkill/index.js
@@ -19,9 +19,16 @@ const STOP_MESSAGE = 'Goodbye!';
 
 const guru_work = "Guru works as an Application developer at Nationwide.";
 const guru_fullName = "His full name is Srisarguru Sridhar. He goes by either guru or batman";
-const guru_launch = "Welcome to About Guru. This skill is to know about guru. If you don't know him well you can get to know him through this skill. What do you like to know about him ?"
-const guru_color = "His favourite colors are red and black. Although he always told me he wanted rainbow dyed hair"
-const guru_summary = "Guru is a software developer with a passion for technology, development and innovation. He strongly believes that learning is a continuous process and that the best way to gain knowledge is not only by learning but also by sharing. He enjoys working on both back end as well as front end with a constant lookout to learn new technologies currently used in the industry. His career path has helped him to develop strong problem-solving, communication, mentoring and leadership skills along with the ability to work both as a team player as well as a solo performer when needed."
+const guru_launch = "Welcome to About Guru. This skill is to know about guru. If you don't know him well you can get to know him through this skill. What do you like to know about him ?";
+const guru_color = "His favourite colors are red and black. Although he always told me he wanted rainbow dyed hair";
+const guru_summary = "Guru is a software developer with a passion for technology, development and innovation. He strongly believes that learning is a continuous process and that the best way to gain knowledge, is not only by learning but also by sharing. He enjoys working on both backend as well as frontend, with a constant lookout to learn new technologies currently used in the industry. His career path has helped him to develop strong problem-solving, communication, mentoring and leadership skills along with the ability to work both as a team player as well as a solo performer when needed.";
+const guru_favActor = "His favourite actor is Rajnikanth, Hugh Jackman and Vikram";
+const guru_relationship = "He is single and No!, we are not in a relationship";
+const guru_favMovie = "His all time favourite movie is The Prestige directed by Christopher Nolan";
+const guru_nationality = "He is Indian. But he resides now in the US";
+const guru_contact = "You can ask him more by sending an email to him. I have sent his email address to your device";
+const guru_email = "s.srisarguru@gmail.com";
+const guru_contactCardTitle = "Guru's Email Id";
 
 //=========================================================================================================================================
 //Editing anything below this line might break your skill.
@@ -54,6 +61,30 @@ const handlers = {
         const speechOutput = guru_summary; 
         this.response.speak(speechOutput);
         this.emit(':responseReady');
+    },
+    'ActorIntent': function () {
+        const speechOutput = guru_favActor; 
+        this.response.speak(speechOutput);
+        this.emit(':responseReady');
+    },
+    'RelationshipIntent': function () {
+        const speechOutput = guru_relationship; 
+        this.response.speak(speechOutput);
+        this.emit(':responseReady');
+    },
+    'MovieIntent': function () {
+        const speechOutput = guru_favMovie; 
+        this.response.speak(speechOutput);
+        this.emit(':responseReady');
+    },
+    'NationalityIntent': function () {
+        const speechOutput = guru_nationality; 
+        this.response.speak(speechOutput);
+        this.emit(':responseReady');
+    },
+     'ContactIntent': function () {
+        const speechOutput = guru_contact; 
+        this.emit(':tellWithCard', speechOutput, guru_contactCardTitle, guru_email);
     },
     'AMAZON.HelpIntent': function () {
         const speechOutput = HELP_MESSAGE;
