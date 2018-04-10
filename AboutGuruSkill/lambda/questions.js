8956565517218c77911cd61c89b95becc744978c
Srisarguru Sridhar <srisargurusridhar@u.boisestate.edu>
2018-04-09 23:01:28 -0400

    Started adding questions for guru trivia game - 20180409

---
+++ AboutGuruSkill/lambda/questions.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    QUESTIONS: [
+        {
+            'What is guru\'s favourite movie?': [
+                'The Prestige',
+                'The Dark Knight',
+                'The Avengers',
+                'Titanic',
+                'Dunkirk',
+                'Kabali',
+                'Logan'
+            ],
+        },
+    ]
+};
